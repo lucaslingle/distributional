c85ea5dc9e736b8dff2cdbc035b9e8adb1925edc
lucaslingle <lucasdaxlingle@gmail.com>
2026-08-09 10:18:08 -0700

    dev-1 : initial rebin reimplementation

--- src/distributional/histogram.py
+++ src/distributional/histogram.py
@@ -140,13 +140,19 @@ class Histogram:
         while (new_vmin < old_vmin):
             old_vmin -= self.atom_stride
             old_left_atom_padding.insert(0, old_vmin)
+        # extra one mandatory padding
+        old_vmin -= self.atom_stride
+        old_left_atom_padding.insert(0, old_vmin)
         old_left_atom_padding = np.array(old_left_atom_padding)
 
         old_right_atom_padding = []
         while (old_vmax < new_vmax):
             old_vmax += self.atom_stride
             old_right_atom_padding.append(old_vmax)
-        old_right_atom_padding = np.array(old_left_atom_padding)
+        # extra one mandatory padding
+        old_vmax += self.atom_stride
+        old_right_atom_padding.append(old_vmax)
+        old_right_atom_padding = np.array(old_right_atom_padding)
 
         old_atoms = np.concatenate([
             old_left_atom_padding,
@@ -162,123 +168,37 @@ class Histogram:
         new_atoms = np.arange(new_num_atoms) * new_atom_stride + new_vmin + new_atom_stride/2
         new_probs = np.zeros(dtype=self.probs.dtype, shape=[new_num_atoms])
         j = 0
-        for i in range(0, self.new_num_atoms):
+        for i in range(0, new_num_atoms):
+            print(f"new i: {i}")
+            # for each new i, we should only get to it once its left side is past
+            # the left side of of old bin j. 
             old_left = old_atoms[j] - self.atom_stride / 2
             old_right = old_atoms[j] + self.atom_stride / 2
             new_left = new_atoms[i] - new_atom_stride / 2
             new_right = new_atoms[i] + new_atom_stride / 2
-            assert old_left <= new_left <= new_right, f"got (old_left, new_left, new_right) == ({old_left}, {new_left}, {new_right})"
-            if new_right <= old_right:
-                frac = (new_right - new_left) / self.atom_stride
+            assert old_left <= new_left, f"got (old_left, new_left) == ({old_left}, {new_left})"
+
+            # now we loop over old bins j and add in the contrib to new bin i
+            while (old_left < new_right):
+                print(f"current i,j: {i},{j}")
+                # compute intersection between current i and j.
+                old_left = old_atoms[j] - self.atom_stride / 2
+                old_right = old_atoms[j] + self.atom_stride / 2
+                print(f"new_left, new_right: {new_left}, {new_right}")
+                print(f"old_left, old_right: {old_left}, {old_right}")
+
+                intersect_left = max(new_left, old_left)
+                intersect_right = min(new_right, old_right)
+                frac = (intersect_right - intersect_left) / self.atom_stride
                 new_probs[i] += frac * old_probs[j]
-            else:
-                frac = (old_right - new_left) / self.atom_stride
-                new_probs[i] += frac * old_probs[j]
-                while (new_right > old_right):
-                    frac = (new_right - old_right) / self.atom_stride
-                    new_probs[i] += frac * (old_probs[j+1] if j+1 < old_probs.shape[0] else 0.0)
+                if old_right < new_right:
                     j += 1
-                    old_left = old_atoms[j] - self.atom_stride / 2
-                    old_right = old_atoms[j] + self.atom_stride / 2
-
-    # todo: redo this
-    def rebin_old(self, num_atoms: int, new_vmin: float, new_vmax: float) -> 'Histogram':
-        old_atoms = self.atoms
-        old_vmin = old_atoms[0] - self.atom_stride / 2
-        old_vmax = old_atoms[-1] + self.atom_stride / 2
-        atom_stride = (new_vmax - new_vmin) / num_atoms
-        print(f"atom_stride: {atom_stride}")
-        assert (new_vmin <= old_vmin), f"missing left bin coverage of old distribution: (new_vmin > old_vmin), ({new_vmin} > {old_vmin})"
-        assert (old_vmax <= new_vmax), f"missing right bin coverage of old distribution: (old_vmax > new_vmax), ({old_vmax} > {new_vmax})"
-
-        old_left_atom_padding = []
-        while (new_vmin < old_vmin):
-            old_vmin -= self.atom_stride
-            old_left_atom_padding.insert(0, old_vmin)
-        old_left_atom_padding = np.array(old_left_atom_padding)
-
-        old_right_atom_padding = []
-        while (old_vmax < new_vmax):
-            old_vmax += self.atom_stride
-            old_right_atom_padding.append(old_vmax)
-        old_right_atom_padding = np.array(old_left_atom_padding)
-
-        old_atoms = np.concatenate([
-            old_left_atom_padding,
-            old_atoms,
-            old_right_atom_padding,
-        ], axis=0)
-        old_probs = np.concatenate([
-            np.zeros_like(old_left_atom_padding),
-            self.probs,
-            np.zeros_like(old_right_atom_padding),
-        ], axis=0)
-
-        new_atoms = np.arange(num_atoms) * atom_stride + new_vmin + atom_stride/2
-        new_probs = np.zeros(dtype=self.probs.dtype, shape=[num_atoms])
-        j = 0
-        for i in range(0, num_atoms):
-            # print(f"i,j: {i}, {j}")
-            old_left = old_atoms[j] - self.atom_stride / 2
-            old_right = old_atoms[j] + self.atom_stride / 2
-            new_left = new_atoms[i] - atom_stride / 2
-            new_right = new_atoms[i] + atom_stride / 2
-            # print(f"old_left, old_right: {old_left}, {old_right}")
-            # print(f"new_left, new_right: {new_left}, {new_right}")
-            assert old_left <= new_left <= new_right, f"got (old_left, new_left, new_right) == ({old_left}, {new_left}, {new_right})"
-
-            if new_right < old_right:
-                frac = (new_right - new_left) / self.atom_stride
-                new_probs[i] += frac * old_probs[j]
-            if (new_right >= old_right):
-                frac = (old_right - new_left) / self.atom_stride
-                new_probs[i] += frac * old_probs[j]
-                frac = (new_right - old_right) / self.atom_stride
-                new_probs[i] += frac * (old_probs[j+1] if j+1 < old_probs.shape[0] else 0.0)
-                j += 1
-                #old_left = old_atoms[j] - self.atom_stride / 2
-                #old_right = old_atoms[j] + self.atom_stride / 2
+                else:
+                    break
 
         return Histogram(
-            first_atom=new_vmin + atom_stride/2,
-            num_atoms=num_atoms,
-            atom_stride=atom_stride,
+            vmin=new_vmin,
+            vmax=new_vmax,
+            num_atoms=new_num_atoms,
             probs=new_probs,
         )
-
-
-if __name__ == "__main__":
-    hist = Histogram(first_atom=0, num_atoms=10, atom_stride=1, probs=np.array([0.1 for _ in range(10)]))
-    hist.plot()
-
-    hist2 = hist * 2.0
-    hist2.plot()
-
-    hist3 = hist * 0.5
-    hist3.plot()
-
-    hist4 = hist3 + 1.0
-    hist4.plot()
-
-    hist5 = hist + hist
-    hist5.plot()
-
-    hist6 = hist.convolve(hist)
-    hist6.plot()
-
-    print(hist5.probs)
-    print(hist6.probs)
-
-    hist10 = hist.rebin(new_vmin=-0.5, new_vmax=9.5, num_atoms=20)
-    hist10.plot()
-    print(np.sum(hist10.probs))
-
-    hist11 = hist.rebin(new_vmin=-0.5, new_vmax=9.5, num_atoms=100)
-    hist11.plot()
-    print(np.sum(hist11.probs))
-
-    # ((hist5 + -10.0) * 0.3).rebin(1000, -100, 100) + ((hist5 + 10.0) * 0.7).rebin(1000, -100, 100)
-
-    # ((hist5 + -10.0) * 0.3).rebin(1000, -100, 100)
-
-    # ((hist5 + -10.0) * 0.3).rebin(100, -20, 20).probs
