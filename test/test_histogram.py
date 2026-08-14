0bc2404833fd0c12a863fb20af2166b8c00f6cf1
lucaslingle <lucasdaxlingle@gmail.com>
2026-08-13 19:46:30 -0700

    update tests, add coverage as dep

--- test/test_histogram.py
+++ test/test_histogram.py
@@ -3,40 +3,46 @@ import pytest
 
 from distributional.histogram import Histogram
 
-@pytest.fixture
-def unif_probs():
-    def func(n):
-        return np.array([1 / n for _ in range(n)])
-    return func
 
-@pytest.fixture
-def unif_histogram(unif_probs):
-    def func(n):
-        return Histogram(0., 1., n, unif_probs(n))
-    return func
+TOLS = dict(atol=1e-4, rtol=1e-4)
 
-def test_histogram_convolve_rel_test(unif_histogram):
-    hist = unif_histogram(6)
-    hist_conv = hist._convolve(hist)
-    hist_conv_slow = hist._convolve_slow(hist)
-    np.testing.assert_allclose(hist_conv.probs, hist_conv_slow.probs)
 
-def test_histogram_rebin_unaligned_low2hi(unif_probs, unif_histogram):
-    hist = unif_histogram(2)
-    hist = hist.rebin(0., 1., 3)
-    np.testing.assert_allclose(unif_probs(3), hist.probs)
+def unif_probs(n):
+    return np.array([1 / n for _ in range(n)])
 
-def test_histogram_rebin_unaligned_hi2low(unif_probs, unif_histogram):
-    hist = unif_histogram(3)
-    hist = hist.rebin(0., 1., 2)
-    np.testing.assert_allclose(unif_probs(2), hist.probs)
 
-def test_histogram_rebin_aligned_low2hi(unif_probs, unif_histogram):
-    hist = unif_histogram(10)
-    hist = hist.rebin(0., 1., 100)
-    np.testing.assert_allclose(unif_probs(100), hist.probs)
+def alt_probs(n):
+    assert n % 2 == 0
+    return np.array([(2 / n if i % 2 == 0 else 0.) for i in range(n)])
 
-def test_histogram_rebin_aligned_hi2low(unif_probs, unif_histogram):
-    hist = unif_histogram(100)
-    hist = hist.rebin(0., 1., 10)
-    np.testing.assert_allclose(unif_probs(10), hist.probs)
+
+def unif_histogram(n):
+    return Histogram(0., 1., n, unif_probs(n))
+
+
+def alt_histogram(n):
+    return Histogram(0., 1., n, alt_probs(n))
+
+
+@pytest.mark.parametrize("hist", [unif_histogram, alt_histogram])
+def test_histogram_convolve_rel_test(hist):
+    h = hist(6)
+    hc = h._convolve(h)
+    hcs = h._convolve_slow(h)
+    np.testing.assert_allclose(hc.probs, hcs.probs, **TOLS)
+
+
+@pytest.mark.parametrize("probs, hist", [(unif_probs, unif_histogram)])
+@pytest.mark.parametrize("start_n, end_n", [(2, 3), (3, 2)])
+def test_histogram_rebin_unaligned(probs, hist, start_n, end_n):
+    h = hist(start_n)
+    h = h.rebin(0., 1., end_n)
+    np.testing.assert_allclose(probs(end_n), h.probs, **TOLS)
+
+
+@pytest.mark.parametrize("probs, hist", [(unif_probs, unif_histogram)])
+@pytest.mark.parametrize("start_n, end_n", [(10, 100), (100, 10)])
+def test_histogram_rebin_aligned(probs, hist, start_n, end_n):
+    h = hist(start_n)
+    h = h.rebin(0., 1., end_n)
+    np.testing.assert_allclose(probs(end_n), h.probs, **TOLS)
